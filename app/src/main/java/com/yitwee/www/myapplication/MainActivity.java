53a3a77768d363799f0083cc298fb2a780054326
cjb <13368224769@163.com>
2016-10-23 14:53:46 +0800

    xiagao

--- app/src/main/java/com/yitwee/www/myapplication/MainActivity.java
+++ app/src/main/java/com/yitwee/www/myapplication/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       /// 测试4655756746
+       /// 测试
         //testing code  weidonghua 14"25
     }
 }
